21888d7d099489f7405958d1e4d15acfb5b5422d
tavisca-tkatyal <tkatyal@tavisca.in>
2019-06-30 00:28:40 +0530

    Add Changes in Program.cs And OutputDigit.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/OutputDigit.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/OutputDigit.cs
@@ -4,59 +4,46 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     class FixMultiplication
     {
+        public int FindDigit(float tempResult,string stringWithUnknownDigit)
+        {
+            string actualNumber = tempResult.ToString();
+            int indexOfUnknownDigit = stringWithUnknownDigit.IndexOf('?');
+            if(actualNumber.Length  < stringWithUnknownDigit.Length || actualNumber.Length > stringWithUnknownDigit.Length)
+            {
+                    return -1;
+            }
+            char outputCharacterContainUnkownDigit = actualNumber[indexOfUnknownDigit];
+            int missingDigit = (int)Char.GetNumericValue(outputCharacterContainUnkownDigit);
+            return missingDigit;
+        }
         public int FindDigit(String equation)
         {
             int prev = equation.IndexOf('*');
-            string stringA = equation.Substring(0,prev);
+            string stringContainValueOfA = equation.Substring(0,prev);
             int next = equation.IndexOf('=');
-            string stringB = equation.Substring(prev+1,next-prev-1);
-            string stringC = equation.Substring(next+1);
-            //Console.WriteLine($"{A} {B} {C}");
-            if(stringC.Contains('?'))
+            string stringConainValueOfB = equation.Substring(prev+1,next-prev-1);
+            string stringContainValueOfC = equation.Substring(next+1);
+            if(stringContainValueOfC.Contains('?'))
             {
-                float A = float.Parse(stringA);
-                float B = float.Parse(stringB);
+                float A = float.Parse(stringContainValueOfA);
+                float B = float.Parse(stringConainValueOfB);
                 float tempResult = A*B;
-                int indexOfUnknownDigit = stringC.IndexOf('?');
-                string actualNumber = tempResult.ToString();
-                if(actualNumber.Length <stringC.Length)
-                {
-                    return -1;
-                }
-                char outputCharacterContainUnkownDigit = actualNumber[indexOfUnknownDigit];
-               // Console.WriteLine(c);
-               int missingDigit = (int)Char.GetNumericValue(outputCharacterContainUnkownDigit);
-                return missingDigit;
+                return FindDigit(tempResult,stringContainValueOfC);
+                
             }
-            else if(stringA.Contains('?'))
+            else if(stringContainValueOfA.Contains('?'))
             {
-                float B = float.Parse(stringB);
-                float C = float.Parse(stringC);
+                float B = float.Parse(stringConainValueOfB);
+                float C = float.Parse(stringContainValueOfC);
                 float tempResult = C/B;
-                int indexOfUnknownDigit = stringA.IndexOf('?');
-                string actualNumber =tempResult.ToString();
-                if(actualNumber.Length<stringA.Length)
-                {
-                    return -1;
-                }
-                char outputCharacterContainUnkownDigit = actualNumber[indexOfUnknownDigit];
-                int missingDigit = (int)Char.GetNumericValue(outputCharacterContainUnkownDigit);
-                return missingDigit;
+                return FindDigit(tempResult,stringContainValueOfA);
             }
-            else if(stringB.Contains('?'))
+            else if(stringConainValueOfB.Contains('?'))
             {
-                float A = float.Parse(stringA);
-                float C = float.Parse(stringC);
+                float A = float.Parse(stringContainValueOfA);
+                float C = float.Parse(stringContainValueOfC);
                 float tempResult = C/A;
-                int indexOfUnknownDigit = stringB.IndexOf('?');
-                string actualNumber = tempResult.ToString();
-                if(actualNumber.Length>stringB.Length || stringB.Length > actualNumber.Length)
-                {
-                    return -1;
-                }
-                char outputCharacterContainUnkownDigit = actualNumber[indexOfUnknownDigit];
-                int missingDigit = (int)Char.GetNumericValue(outputCharacterContainUnkownDigit);
-                return missingDigit;
+                return FindDigit(tempResult,stringConainValueOfB);
             }
             throw new NotImplementedException();   
         }
--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,59 +22,9 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            int prev = equation.IndexOf('*');
-            string A = equation.Substring(0,prev);
-            int next = equation.IndexOf('=');
-            string B = equation.Substring(prev+1,next-prev-1);
-            string C = equation.Substring(next+1);
-            //Console.WriteLine($"{A} {B} {C}");
-            if(C.Contains('?'))
-            {
-                float a = float.Parse(A);
-                float b = float.Parse(B);
-                float res = a*b;
-                int index = C.IndexOf('?');
-                string final =res.ToString();
-                if(final.Length <C.Length)
-                {
-                    return -1;
-                }
-                char c = final[index];
-               // Console.WriteLine(c);
-               int ff = (int)Char.GetNumericValue(c);
-                return ff;
-            }
-            else if(A.Contains('?'))
-            {
-                float b = float.Parse(B);
-                float c = float.Parse(C);
-                float res = c/b;
-                int index = A.IndexOf('?');
-                string final =res.ToString();
-                if(final.Length<A.Length)
-                {
-                    return -1;
-                }
-                char d = final[index];
-                int ff = (int)Char.GetNumericValue(d);
-                return ff;
-            }
-            else if(B.Contains('?'))
-            {
-                float a = float.Parse(A);
-                float c = float.Parse(C);
-                float res = c/a;
-                int index = B.IndexOf('?');
-                string final =res.ToString();
-                if(final.Length>B.Length || B.Length > final.Length)
-                {
-                    return -1;
-                }
-                char d = final[index];
-                int ff = (int)Char.GetNumericValue(d);
-                return ff;
-            }
-            throw new NotImplementedException();   
+           FixMultiplication objectOfFixMultiplication  = new FixMultiplication();
+           int missingDigit = objectOfFixMultiplication.FindDigit(equation);
+           return missingDigit;
         }
     }
 }
